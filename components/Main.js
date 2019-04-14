7c513019fb7c543dfd797fe99455bdcfeb6c5088
Frugence Fidel <frugencefidel@gmail.com>
2019-04-14 18:18:34 +0300

    Initial commit

---
+++ components/Main.js
@@ -0,0 +1,13 @@
+const Main = () => (
+  <main>
+    <h1>World Designer Event 2018</h1>
+    <h2>28-31 May 2019 | Izmir, Turkey</h2>
+    <p>
+      Aliquam rutrum pellentesque semper. Maecenas sit amet ante iaculis sapien
+      feugiat tristique.
+    </p>
+    <button>Book Now</button>
+  </main>
+);
+
+export default Main;
