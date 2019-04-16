152da291f3dc666aa778d0e8245ae2c53f973d2a
Frugence Fidel <frugencefidel@gmail.com>
2019-04-16 20:18:03 +0300

    Add theme provider

--- components/Page.js
+++ components/Page.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
 import Meta from './Meta';
+import Navbar from './Navbar';
+
+const theme = {
+  blueCool3: '#3E4C59',
+  blueCool10: '#F5F7FA'
+};
+
+const StyledPage = styled.div`
+  color: ${props => props.theme.blueCool3};
+  background: ${props => props.theme.blueCool10};
+`;
 
 class Page extends Component {
   render() {
     return (
-      <>
-        <Meta />
-        <Navbar />
-        {this.props.children}
-        <p>Footer Goes here</p>
-      </>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Navbar />
+          {this.props.children}
+          <p>Footer Goes here</p>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
