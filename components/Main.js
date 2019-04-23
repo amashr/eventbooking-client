383d5f68cb8f0ad21b5f0d5402ef957ad8a8c44c
Frugence <frugencefidel@gmail.com>
2019-04-23 12:12:31 +0300

    Modify main section title

--- components/Main.js
+++ components/Main.js
@@ -1,34 +1,39 @@
 import styled from 'styled-components';
 
 const StyledMain = styled.main`
-  background-image: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.2),
-      rgba(0, 0, 0, 0.5)
-    ),
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
     url('/static/main.jpg');
   background-blend-mode: saturation;
   background-repeat: no-repeat;
   background-size: cover;
-  background-position-y: -2px;
-  height: 400px;
-  padding-top: 100px;
+  background-position-y: -0.2rem;
+  height: 40rem;
+  padding-top: 10rem;
   color: ${props => props.theme.blueCool10};
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const StyledMainWrapper = styled.div`
+  width: 100rem;
+
+  h1 {
+    font-size: 50px;
+    margin-right: 20rem;
+  }
 `;
 
 const Main = () => (
   <StyledMain>
-    <h1>World Designer Event 2018</h1>
-    <h2>28-31 May 2019 | Izmir, Turkey</h2>
-    <p>
-      Aliquam rutrum pellentesque semper. Maecenas sit amet ante iaculis sapien
-      feugiat tristique.
-    </p>
+    <StyledMainWrapper>
+      <h1>Aliquam rutrum pellentesque sempere.</h1>
 
-    <div>
-      <input type="text" placeholder="Search event by keyword " />
-      <button>search icon</button>
-    </div>
+      <div>
+        <input type="text" placeholder="Search event by keyword " />
+        <button>search icon</button>
+      </div>
+    </StyledMainWrapper>
   </StyledMain>
 );
 
