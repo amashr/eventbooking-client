60ca41046e082123977898ccc5009c4e2d739c03
Frugence <frugencefidel@gmail.com>
2019-04-24 14:21:12 +0300

    Add hover effect

--- components/Btn.js
+++ components/Btn.js
@@ -2,17 +2,27 @@ import styled from 'styled-components';
 import Link from 'next/link';
 
 const StyledBtn = styled.a`
-  border: 0.2rem solid ${props => props.theme.red};
-  color: ${props => props.theme.red};
-  border-radius: 0.4rem;
-  padding: 1.2rem 3rem;
-  cursor: pointer;
-  background: transparent;
-  text-transform: uppercase;
-  text-align: center;
-  font-size: 1.25rem;
-  display: flex;
-  align-items: center;
+  &,
+  &:visited {
+    border: 0.2rem solid ${props => props.theme.red};
+    color: ${props => props.theme.red};
+    border-radius: 0.4rem;
+    padding: 1.2rem 3rem;
+    cursor: pointer;
+    background: transparent;
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 1.25rem;
+    display: flex;
+    align-items: center;
+    transition: all 0.2s;
+  }
+
+  &:hover,
+  &:active {
+    color: ${props => props.theme.blueCool10};
+    background: ${props => props.theme.red};
+  }
 `;
 
 const Btn = props => (
