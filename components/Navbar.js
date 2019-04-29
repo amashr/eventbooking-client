9050ec959933b09de495a730ef2425dc63d1e368
Frugence <frugencefidel@gmail.com>
2019-04-29 18:56:07 +0300

    Change color on scroll page Yoffset

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,8 +5,9 @@ import styled from 'styled-components';
 import Btn from './Btn';
 
 const StyledNavbar = styled.nav`
-  background: transparent;
-  position: absolute;
+  background: ${props =>
+    props.isScroll ? 'var(--blueCool10)' : 'transparent'};
+  position: fixed;
   top: 0;
   width: 100%;
   padding: 1rem;
@@ -95,7 +96,8 @@ const NavbarLinkContainer = styled.ul`
     .link {
       &,
       &:visited {
-        color: var(--blueCool10);
+        color: ${props =>
+          props.isScroll ? 'var(--blueCool3)' : 'var(--blueCool10)'};
         transition: var(--transition);
       }
 
@@ -113,9 +115,24 @@ const NavbarLinkContainer = styled.ul`
 
 class Navbar extends Component {
   state = { isScroll: false };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50) {
+      this.setState({ isScroll: true });
+    } else {
+      this.setState({ isScroll: false });
+    }
+  };
+
   render() {
     return (
-      <StyledNavbar>
+      <StyledNavbar isScroll={this.state.isScroll}>
         <Logo>
           <Link href="/">
             <a>Event Booking</a>
@@ -134,7 +151,7 @@ class Navbar extends Component {
         </NavbarMenu>
 
         <NavbarItem>
-          <NavbarLinkContainer>
+          <NavbarLinkContainer isScroll={this.state.isScroll}>
             <li>
               <Link href="/concerts">
                 <a className="link">Concerts</a>
@@ -162,7 +179,7 @@ class Navbar extends Component {
             </li>
           </NavbarLinkContainer>
 
-          <NavbarLinkContainer>
+          <NavbarLinkContainer isScroll={this.state.isScroll}>
             <li>
               <Link href="/login">
                 <a className="link">Sign In</a>
@@ -177,67 +194,5 @@ class Navbar extends Component {
     );
   }
 }
-
-// const Navbar = () => (
-//   <StyledNavbar>
-//     <Logo>
-//       <Link href="/">
-//         <a>Event Booking</a>
-//       </Link>
-//     </Logo>
-
-//     <NavbarMenu>
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         width="24"
-//         height="24"
-//         viewBox="0 0 24 24"
-//       >
-//         <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
-//       </svg>
-//     </NavbarMenu>
-
-//     <NavbarItem>
-//       <NavbarLinkContainer>
-//         <li>
-//           <Link href="/concerts">
-//             <a className="link">Concerts</a>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link href="/sports">
-//             <a className="link">Sports</a>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link href="/arts">
-//             <a className="link">Arts &amp; Theater</a>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link href="/">
-//             <a className="link">For You</a>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link href="/">
-//             <a className="link">More</a>
-//           </Link>
-//         </li>
-//       </NavbarLinkContainer>
-
-//       <NavbarLinkContainer>
-//         <li>
-//           <Link href="/login">
-//             <a className="link">Sign In</a>
-//           </Link>
-//         </li>
-//         <li>
-//           <Btn path="/">Get Started</Btn>
-//         </li>
-//       </NavbarLinkContainer>
-//     </NavbarItem>
-//   </StyledNavbar>
-// );
 
 export default Navbar;
