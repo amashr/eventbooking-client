d29c995a2ba923b27bc698af1e9c2bdf89a40f3c
Frugence <frugencefidel@gmail.com>
2019-05-01 21:42:53 +0300

    Add content event banner

--- components/EventCreate.js
+++ components/EventCreate.js
@@ -1,6 +1,29 @@
+import Link from 'next/link';
+
 const EventCreate = () => (
   <section>
-    <p>Create Event</p>
+    <div>
+      <div>
+        <h4>Want to create an event?</h4>
+      </div>
+      <div>
+        <Link href="/">
+          <a>
+            Learn more
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+              </svg>
+            </span>
+          </a>
+        </Link>
+      </div>
+    </div>
   </section>
 );
 
