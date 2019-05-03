e1d44a215d88130e02bbbbf98c7470904eb0a3c7
Frugence <frugencefidel@gmail.com>
2019-05-03 22:48:19 +0300

    Style card

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,45 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+  border: 1px solid black;
+  max-width: 36rem;
+
+  display: flex;
+  flex-direction: column;
+
+  a {
+    img {
+      width: 100%;
+      height: 17rem;
+      object-fit: cover;
+    }
+  }
+`;
+
+const CardContent = styled.div`
+  /* flex: 1 50%; */
+`;
+
 const Card = props => (
-  <div>
-    <img src="/static/event.jpg" alt="Event" />
-    <h4>{props.title}</h4>
-    <p>{props.date}</p>
-    <p>{props.location}</p>
-    <p>{props.price}</p>
-  </div>
+  <StyledCard>
+    <Link href="/">
+      <a>
+        <img src="/static/event.jpg" alt={props.title} />
+      </a>
+    </Link>
+
+    <CardContent>
+      <h4>
+        <Link href="/">
+          <a>{props.title}</a>
+        </Link>
+      </h4>
+      <p>{props.date}</p>
+      <p>{props.location}</p>
+      <p>{props.price}</p>
+    </CardContent>
+  </StyledCard>
 );
 
 export default Card;
