37e49df73c32babe47411deed5a9f21a3b91ad09
Frugence <frugencefidel@gmail.com>
2019-05-03 20:31:17 +0300

    Add events content

--- components/Events.js
+++ components/Events.js
@@ -1,6 +1,24 @@
+import Link from 'next/link'
+
 const Events = () => (
   <section>
-    <p>Events goes here</p>
+    <div>
+      <h2>Top Selling</h2>
+      <div>
+        <h3>Concerts</h3>
+        <Link href=""><a>See All Concerts</a></Link>
+      </div>
+
+      <div>
+        <div>
+          <img src="/static/event.jpg" alt="Event" />
+          <h4>Event Title</h4>
+          <p>Thu, May 16, 9:00am</p>
+          <p>Rock City Mall, Mwanza, Tanzania</p>
+          <p>Free</p>
+        </div>
+      </div>
+    </div>
   </section>
 );
 
