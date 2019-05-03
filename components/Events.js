2882023a4259d107c21e77a179ee2d284fc6f3c2
Frugence <frugencefidel@gmail.com>
2019-05-04 02:00:34 +0300

    Style events

--- components/Events.js
+++ components/Events.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
 import Card from './Card';
+
+const StyledEvents = styled.section`
+  padding: 7rem 0;
+`;
+
+const Wrapper = styled.div`
+  max-width: 116rem;
+  margin: 0 auto;
+
+  h2 {
+    font-size: 3rem;
+  }
+`;
+
+const EventSubtitle = styled.div`
+  padding: 2rem 0 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const EventsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  & > div:not(:nth-child(3n)) {
+    flex-basis: calc(100% / 3);
+    margin-right: 2rem;
+  }
+`;
 
 class Events extends Component {
   state = {
@@ -53,30 +86,31 @@ class Events extends Component {
 
   render() {
     return (
-      <section>
-        <div>
+      <StyledEvents>
+        <Wrapper>
           <h2>Top Selling</h2>
-          <div>
+          <EventSubtitle>
             <h3>Concerts</h3>
             <Link href="">
               <a>See All Concerts</a>
             </Link>
-          </div>
+          </EventSubtitle>
 
-          <div>
-            {this.state.events.map(event => {
+          <EventsWrapper>
+            {this.state.events.map((event, key) => {
               return (
                 <Card
                   title={event.title}
                   date={event.date}
                   location={event.location}
                   price={event.price}
+                  key={key}
                 />
               );
             })}
-          </div>
-        </div>
-      </section>
+          </EventsWrapper>
+        </Wrapper>
+      </StyledEvents>
     );
   }
 }
