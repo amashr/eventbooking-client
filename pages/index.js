f9989f647b99fbe17955870634d82e91365241d9
Frugence <frugencefidel@gmail.com>
2019-06-07 16:33:37 +0300

    Remove events state

--- pages/index.js
+++ pages/index.js
@@ -7,50 +7,6 @@ import Events from '../components/Events';
 
 class Home extends Component {
   state = {
-    events: [
-      {
-        title: 'Event1',
-        img: '/static/event.jpg',
-        date: 'Thu, May 16, 9:00am',
-        location: 'Rock City Mall, Mwanza, Tanzania',
-        price: 'free'
-      },
-      {
-        title: 'Event2',
-        img: '/static/event.jpg',
-        date: 'Thu, May 26, 7:30am',
-        location: 'Coco Beach, Dar es salaam, Tanzania',
-        price: '$200'
-      },
-      {
-        title: 'Event3',
-        img: '/static/event.jpg',
-        date: 'Thu, May 26, 7:30am',
-        location: 'Coco Beach, Dar es salaam, Tanzania',
-        price: 'free'
-      },
-      {
-        title: 'Event4',
-        img: '/static/event.jpg',
-        date: 'Thu, May 26, 7:30am',
-        location: 'Coco Beach, Dar es salaam, Tanzania',
-        price: 'free'
-      },
-      {
-        title: 'Event5',
-        img: '/static/event.jpg',
-        date: 'Thu, May 26, 7:30am',
-        location: 'Coco Beach, Dar es salaam, Tanzania',
-        price: 'free'
-      },
-      {
-        title: 'Event6',
-        img: '/static/event.jpg',
-        date: 'Thu, May 26, 7:30am',
-        location: 'Coco Beach, Dar es salaam, Tanzania',
-        price: 'free'
-      }
-    ],
     eventsCategories: ['Concerts', 'Sports', 'Arts & Theater', 'Family']
   };
 
