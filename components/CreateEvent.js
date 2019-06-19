c12f28ee80d74f835cbd42710c5ee8d9becb0c80
FrugenceFide <frugencefidel@gmail.com>
2019-06-19 11:47:44 +0300

    Add create event page and modify .gitignore

---
+++ components/CreateEvent.js
@@ -0,0 +1,12 @@
+const CreateEvent = () => (
+  <div>
+    <form action="">
+      <label htmlFor="title">
+        Title
+        <input type="text" id="title" placeholder="Title" required />
+      </label>
+    </form>
+  </div>
+);
+
+export default CreateEvent;
